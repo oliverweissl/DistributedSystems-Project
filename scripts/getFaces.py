97ed141aa8f65d11688754798160c0896f7243b3
oliverweissl <o.weissl@student.vu.nl>
2022-11-12 13:21:09 +0100

    scripts + basic code

---
+++ scripts/getFaces.py
@@ -0,0 +1,13 @@
+import json
+
+
+def handler_name(event, context):
+    var = event["body"]
+
+    # Do something
+
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type:", "application/json"},
+        "body": json.dumps(var)
+    }
