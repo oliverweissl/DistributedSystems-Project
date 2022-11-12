55ceee351ffeff3637e05ada2868ae872c843e18
oliverweissl <o.weissl@student.vu.nl>
2022-11-12 14:08:55 +0100

    first functionality for fetchImage/loadBatch

--- scripts/fetchImage.py
+++ scripts/fetchImage.py
@@ -1,13 +1,20 @@
+import boto3
+import math
 import json
 
+BATCH_SIZE = 64
+s3 = boto3.resource('s3')
 
 def handler_name(event, context):
-    var = event["body"]
-
-    # Do something
+    bucket_url = event["body"] # access bucket URL in body
+    bucket = s3.Bucket(bucket_url) # define bucket to access
+    image_keys = [obj.key for obj in bucket.objects.all()] # get all keys of objects in bucket
 
     return {
         "statusCode": 200,
         "headers": {"Content-Type:", "application/json"},
-        "body": json.dumps(var)
+        "body": json.dumps({"bucket": bucket_url,
+                            "image_keys":image_keys,
+                            "batch_size": BATCH_SIZE
+                            })
     }
--- scripts/loadBatch.py
+++ scripts/loadBatch.py
@@ -1,13 +1,29 @@
 import json
+import boto3
 
+s3 = boto3.resource("s3")
 
 def handler_name(event, context):
-    var = event["body"]
+    bucket_url = event["bucket"]
+    image_keys = event["image_keys"]
+    batch_size = event["batch_size"]
 
-    # Do something
+    bucket = s3.Bucket(bucket_url)  # define bucket to access
+    available_keys = [obj.key for obj in bucket.objects.all()]  # get all keys of objects in bucket
 
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type:", "application/json"},
-        "body": json.dumps(var)
-    }
+    for i in range(0,len(available_keys),batch_size):
+        batch_selection = image_keys[i:i+batch_size]
+        if batch_selection in available_keys: # if keys not loaded yet
+            [s3.put_object(Bucket=bucket_url, Key=f"loaded/{obj}") for obj in batch_selection] # mark keys as loaded + break
+            batch_selection = [f"loaded/{obj}" for obj in batch_selection]
+            return {
+                "statusCode": 200,
+                "headers": {"Content-Type:", "application/json"},
+                "body": json.dumps({"bucket": bucket_url,
+                            "batch_keys": batch_selection
+                            })
+            }
+        else: raise Exception("Batch not loaded: objects not in Bucket")
+
+
+
