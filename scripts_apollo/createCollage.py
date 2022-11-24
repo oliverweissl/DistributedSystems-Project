2e0df5e45213714f70e470797f078f0dfd3c8161
oliverweissl <o.weissl@student.vu.nl>
2022-11-24 16:54:21 +0100

    fixed createCollage

--- scripts_apollo/createCollage.py
+++ scripts_apollo/createCollage.py
@@ -1,6 +1,5 @@
 import io
 import json
-import sys
 import math
 import boto3
 
@@ -14,7 +13,8 @@ def create_collage(image_array: list, sizes: int) -> Image.Image:
     for i in range(grid_size):
         for j in range(grid_size):
             index = i + j * grid_size  # current index of instance
-            temp.paste(image_array[index], (i * sizes, j * sizes))  # add face to temp image
+            if index < len(image_array):
+                temp.paste(image_array[index], (i * sizes, j * sizes))  # add face to temp image
     return temp
 
 def lambda_handler(event, context):
@@ -22,27 +22,23 @@ def lambda_handler(event, context):
     client = boto3.client('s3')
 
     json_input = json.loads(event["body"])
-    emotions = json_input["emotions"]
+    emotions = json_input["emotion"]
     bucket_url = json_input["bucket"]
     face_size = json_input["face_size"]
 
     bucket = s3.Bucket(bucket_url)
-    for emotion in emotions:
+    images = [Image.open(io.BytesIO(obj.get()['Body'].read())) for obj in bucket.objects.filter(Prefix = f"{emotion}/")]
 
-        images = [Image.open(io.BytesIO(s3.Object(bucket_name=bucket, key=obj.key).get()["Body"].read()))
-                  for obj in bucket.objects.filter(Prefix = f"{emotion}/")]
+    if len(images) > 0:
+        collage = create_collage(images, face_size)
+        in_mem_file = io.BytesIO()
+        collage.save(in_mem_file, format="png")
+        in_mem_file.seek(0)
 
-        if len(images) > 0:
-            collage = create_collage(images, face_size)
-            in_mem_file = io.BytesIO()
-            collage.save(in_mem_file, format="png")
-            in_mem_file.seek(0)
-
-            client.upload_fileobj(
-                in_mem_file,
-                bucket,
-                f"{emotion}_collage.png"
-            )
+        client.upload_fileobj(
+            in_mem_file,
+            bucket_url,
+            f"{emotion}_collage.png")
 
 
 
