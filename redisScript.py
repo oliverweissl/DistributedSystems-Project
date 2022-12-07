0bad2e8afc044ab35a34f28a6ae1e5efbdf9f97d
oliverweissl <o.weissl@student.vu.nl>
2022-12-07 11:38:45 +0100

    finnished redis script

--- redisScript.py
+++ redisScript.py
@@ -1,22 +1,35 @@
 import redis
+import random
 
 # redis connection --> change host
-r = redis.Redis(host='localhost', port=6379, db=0, password="insert")
+
+r = redis.Redis(host=input("Enter Host: "), port=6379, db=0, password=input("Enter Password: "))
+
+# sets 10 random entries
+for i in range(10):
+    r.set(random.randbytes(15), f"entry_{i}")
 
 
 # get values by key
-def retrieve_entry(keys: list) -> list:
+def retrieve_entry(keys) -> list:
     values = []
     for key in keys:
+        print(key)
         values.append(r.get(key))
     return values
 
 
-# sets 10 random entries
-for i in range(10):
-    r.set(id(i), f"entry_{i}")
+def del_all(keys):
+    for key in keys:
+        r.delete(key)
+
 
 # retrives all keys on redis cluster
 keys = r.scan_iter()
+
+print(f"Values: {retrieve_entry(keys=keys)}")
+print(f"Keys: {' '.join(map(str,keys))}")
+
+del_all(keys=keys)
 
 
