59f5e637923085766b066dddc163ed814eeaa945
oliverweissl <o.weissl@student.vu.nl>
2022-12-13 20:08:22 +0100

    updated

--- redisImageOps.py
+++ redisImageOps.py
@@ -1,5 +1,4 @@
 import io
-import os
 import json
 import redis
 import base64
@@ -8,37 +7,45 @@ from PIL import Image
 
 FILE_DIR = "IMAGES"
 
-
-def get_image(key: str, c: redis.cluster.RedisCluster):
-    if data := c.get(key):
-        image = data["image"]
-        w, h = data["width"], data["height"]
-        Image.frombytes(image, w, h).save(f"{key}.jpg", format="jpg")
+HOST = "44.203.194.143"
+PW = "testpw"
 
 
-def upload_image(dir: str, c: redis.cluster.RedisCluster):
-    key, fmt = dir.split("/")[-1].split(".")
+def get_image(key: str, c):
+    if data := json.loads(c.get(key)):
+        image = base64.b64decode(data["image"].encode('utf-8'))
+        Image.open(io.BytesIO(image)).save(f"{key}.jpg")
+
+
+def upload_image(dir: str, c):
+    key, *_ = dir.split("/")[-1].split(".")
+
     in_mem_file = io.BytesIO()
     img = Image.open(dir)
-    w, h = img.size()
-    img.save(in_mem_file, format=fmt)
-    base64_enc = base64.b64decode(in_mem_file.seek(0).read())
+    img.save(in_mem_file, format="jpeg")
+    base64_enc = base64.b64encode(in_mem_file.getvalue()).decode("utf-8")
 
-    response = json.dumps({
-        "image": base64_enc,
-        "width": w,
-        "height": h
-    })
+    response = json.dumps({"image": base64_enc})
     c.set(key, response)
 
 
-host_ip = input("Enter Host: ")
-password_str = input("Enter Password: ")
+#host_ip = input("Enter Host: ")
+#password_str = input("Enter Password: ")
 
-c = redis.cluster.RedisCluster(host=host_ip, password=password_str, port=7000)
-value_pairs = c.scan_iter(target_nodes=c.ALL_NODES)
+c = redis.Redis(host=HOST, password=PW)
+#c = redis.cluster.RedisCluster(host=host_ip, password= password_str, port=7000)
 
 
+"""
+#upload images
 images = [f"{FILE_DIR}/{file}" for file in os.listdir(FILE_DIR)]
-print(images)
-print(value_pairs)
+for image in images:
+    upload_image(dir=image, c=c)
+
+
+#download images
+value_pairs = c.scan_iter()
+keys = c.keys()
+for val, key in zip(value_pairs, keys):
+    get_image(key= key, c=c)
+"""
