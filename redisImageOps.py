cc59de4e6424a67dc2e9739ec8f9990e575357b9
oliverweissl <o.weissl@student.vu.nl>
2022-12-23 13:09:43 +0100

    redis working

--- redisImageOps.py
+++ redisImageOps.py
@@ -1,20 +1,21 @@
 import io
+import os
 import json
 import redis
 import base64
 from PIL import Image
 
-
 FILE_DIR = "IMAGES"
+HOST = "3.93.179.171"
 
-HOST = "44.203.194.143"
+NODES = [redis.cluster.ClusterNode(host=HOST, port=7000+port) for port in range(6)]
 PW = "testpw"
 
 
 def get_image(key: str, c):
     if data := json.loads(c.get(key)):
         image = base64.b64decode(data["image"].encode('utf-8'))
-        Image.open(io.BytesIO(image)).save(f"{key}.jpg")
+        Image.open(io.BytesIO(image)).save(f'{key}.jpg')
 
 
 def upload_image(dir: str, c):
@@ -32,20 +33,18 @@ def upload_image(dir: str, c):
 #host_ip = input("Enter Host: ")
 #password_str = input("Enter Password: ")
 
-c = redis.Redis(host=HOST, password=PW)
-#c = redis.cluster.RedisCluster(host=host_ip, password= password_str, port=7000)
-
-
+#c = redis.Redis(host=HOST, password=PW)
+c = redis.cluster.RedisCluster(host=HOST, password= PW, port=7000, startup_nodes=NODES)
+#print(c.keys(target_nodes=NODES))
 """
 #upload images
 images = [f"{FILE_DIR}/{file}" for file in os.listdir(FILE_DIR)]
 for image in images:
     upload_image(dir=image, c=c)
-
+"""
 
 #download images
-value_pairs = c.scan_iter()
-keys = c.keys()
+value_pairs = c.scan_iter(target_nodes=NODES)
+keys = c.keys(target_nodes=NODES)
 for val, key in zip(value_pairs, keys):
     get_image(key= key, c=c)
-"""
